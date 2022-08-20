19a37e2f787b159d1df98f87cc8bdfcbb91831da
RustamAkifli <rustamakifli2003@icloud.com>
2022-08-20 21:21:38 +0400

    search is ready

--- courses/urls.py
+++ courses/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from courses.views import get_courses
+from courses import views as template_views
 # ,courses
 
 urlpatterns = [
     path('courses/', get_courses, name='courses'),
+    path('search/', template_views.SearchView.as_view(), name="search"),
     # path('courses/<int:pk>', courses, name='single_courses'),
 
 ]
