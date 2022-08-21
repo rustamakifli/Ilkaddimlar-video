3a3a8b1f9a0cfe366a5a6e6c6f6580add5b984a8
RustamAkifli <rustamakifli2003@icloud.com>
2022-08-21 12:25:24 +0400

    paginatior is ready and views v1.1

--- courses/views.py
+++ courses/views.py
@@ -1,54 +1,42 @@
-from unicodedata import category
+
 from django.shortcuts import render
 from courses.models import Course,Category
 from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from django.db.models import Q
-
+from django.core.paginator import Paginator
 from courses.models import Course, Chapter
-from django.db.models import Sum
 
 
-def get_courses(request):
-    queryset = Course.objects.all()
-    categories = Category.objects.all()
-    
+def PaginatorCourseList(request):
+    course_list = Course.objects.filter(
+        is_active=True).order_by('created_at')
+    course_len = Course.objects.filter(is_active=True).count()
+
+    paginator = Paginator(course_list, 9)
+
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    if request.GET:
+        course_list = Course.objects.order_by('created_at')
+        # if request.GET.get("category_name"):
+        #     course_list = course_list.filter(
+        #         category__parent__title=request.GET.get("category_name"))
+ 
+
+        paginator = Paginator(course_list, 9)
+        page_number = request.GET.get('page')
+        page_obj = paginator.get_page(page_number)
+
     context = {
-        'title': 'This is test',
-        'categories': categories,
-        'courses': queryset,
+        'page_obj': page_obj,
+        'title': 'Course-list ',
+        'categories': Category.objects.all(),
+        'course_len': course_len,
+
     }
     return render(request, 'course-list.html', context=context)
 
-class CourseListView(ListView):
-    template_name = 'course-list.html'
-    model = Course
-    # context_object_name = 'courses'
-    paginate_by = 9
-
-    def get_queryset(self):
-        queryset = super().get_queryset()
-        queryset = queryset.filter(is_active=False)
-        category_id = self.request.GET.get('category_id') 
-        tag_id = self.request.GET.get('tag_id') 
-        discount = self.request.GET.get('discount') 
-
-
-        if category_id:
-            queryset = queryset.filter(product__category__id=category_id)
-        if tag_id:
-            queryset = queryset.filter(product__tags__id=tag_id)
-        if discount:
-            queryset = queryset.filter(discount=True)
-
-        return queryset
-
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['categories'] = Category.objects.all()
-        context['courses'] = Course.objects.all()
-
-        return context
 
 class SearchView(ListView):
     model = Course
