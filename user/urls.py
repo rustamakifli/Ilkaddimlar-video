fd9803673de90ff5b518ff3d2ea24471c88b88d3
elnurnurulu <elnurnurulu@gmail.com>
2022-09-24 16:19:01 +0400

    login/account buttons

--- user/urls.py
+++ user/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('', template_views.UserLoginView.as_view(), name='login'),
     path('logout/', template_views.logout, name='logout'),
     path('account/', template_views.account, name='account'),
+    path('register/', template_views.register, name='register'),
     # path('detail/<int:pk>', template_views.UserDetailView.as_view(), name='user_detail'),
 ]
--- user/views.py
+++ user/views.py
@@ -5,8 +5,8 @@ from django.contrib.auth import logout as django_logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import get_user_model
 from user.forms import  LoginForm
-from django.contrib.auth.views import LoginView
-
+from django.contrib.auth.views import LoginView, LogoutView
+from django.views.generic import CreateView as RegisterView
 
 from user.forms import (
         LoginForm,
@@ -20,26 +20,28 @@ USER = get_user_model()
 class UserLoginView(LoginView):
     form_class = LoginForm
     template_name = 'login.html'
-    # success_url = reverse_lazy('courses')
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['login_form'] = LoginForm()
-        print(' this works')
         return context
 
     def dispatch(self, request, *args, **kwargs):
         if request.user.is_authenticated:
-            return redirect('courses')
+            return redirect('home')
         return super().dispatch(request, *args, **kwargs)
 
 
 @login_required
 def logout(request):
     django_logout(request)
-    return redirect('courses')
+    return redirect('home')
 
 
 @login_required
 def account(request):
     return render(request, "my-account.html")
+
+
+def register(request):
+    return render(request, "register.html")
