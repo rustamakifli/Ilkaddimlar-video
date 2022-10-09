258cd70cc6ca03de11205011fddee50284d0b4ee
elnurnurulu <elnurnurulu@gmail.com>
2022-10-09 14:50:18 +0400

    user image field is ok

--- core/urls.py
+++ core/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from core import views as template_views
 
 urlpatterns = [
-    path('home/', template_views.HomeView.as_view(), name="home"),
+    path('', template_views.HomeView.as_view(), name="home"),
     path('contact/', template_views.ContactView.as_view(), name="contact"),
 ]
