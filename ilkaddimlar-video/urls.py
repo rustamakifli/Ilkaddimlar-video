a7152b91c37f3fb3c18efa14ca59fb0f2a6f61ac
Rustam Akifli <rustam.a@labrin.net>
2022-10-14 00:40:39 +0400

    smth happened

--- ilkaddimlar-video/urls.py
+++ ilkaddimlar-video/urls.py
@@ -34,4 +34,9 @@ urlpatterns = [
     path('ckeditor/', include(
         'ckeditor_uploader.urls')),
 
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
--- order/urls.py
+++ order/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from order.views import card,checkout,SuccessView,CancelView,CartPageView,wishlist
+from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
-    path('cart/', CartPageView.as_view(), name='cart'),
+    path('cart/', login_required(CartPageView.as_view()), name='cart'),
     path('checkout/<pk>/',checkout,name='checkout'),
     path('success/',SuccessView.as_view(),name='success'),
     path('cancel/',CancelView.as_view(),name='cancel'),
